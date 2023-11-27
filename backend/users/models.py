46406b38a916c8692defcf9b0e8f5e506c88396a
Наталья Арлазарова <sic15@yandex.ru>
2023-11-27 22:38:27 +0300

    Правки в моделях

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 
-from skills.models import Specialization, Skill, PostLevel
+from skills.models import Specialization, Skill
 
 
 #User = get_user_model()
@@ -23,21 +23,16 @@ class UserProfile(models.Model):
                              verbose_name="Пользователь",
                              related_name="profile_user",
                              on_delete=models.CASCADE)
-    specialization = models.ForeignKey(Specialization,
+    current_specialization = models.ForeignKey(Specialization,
                                        related_name='user_specialization',
-                                       verbose_name='Сфера работы',
+                                       verbose_name='Текущий уровень',
                                        on_delete=models.PROTECT,)
     skills = models.ManyToManyField(Skill, through='UserSkills',
                                     verbose_name='Скиллы пользователя',
                                     related_name='user_skills')
-    current_post_level = models.ForeignKey(PostLevel,
-                                           verbose_name='Текущий уровень должности', 
-                                           related_name='current_level',
-                                           on_delete=models.PROTECT,
-                                           default=0)
-    goal_post_level = models.ForeignKey(PostLevel,
-                                        verbose_name='Желаемый уровень должности',
-                                        related_name='goal_level',
+    goal_specialization = models.ForeignKey(Specialization,
+                                        verbose_name='Желаемый уровень',
+                                        related_name='user_goal',
                                         on_delete=models.SET_DEFAULT,
                                         default=0)
     
