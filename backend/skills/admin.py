e469e44ace1f6845dc8d1ffef0395434452c307c
Наталья Арлазарова <sic15@yandex.ru>
2023-11-27 22:14:54 +0300

    правки по замечаниям Валерии

--- backend/skills/admin.py
+++ backend/skills/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from skills.models import ResourceLibrary, SkillGroup, Specialization, PostLevel, Skill
+from skills.models import ResourceLibrary, SkillGroup, Specialization, Skill
 
-# Register your models here.
+
 admin.site.register(ResourceLibrary)
 admin.site.register(SkillGroup)
-
 admin.site.register(Specialization)
 admin.site.register(Skill)
 
-admin.site.register(PostLevel)
--- backend/skills/models.py
+++ backend/skills/models.py
@@ -32,18 +32,7 @@ class SkillGroup(models.Model):
 
    
 class Specialization(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название")
-    code = models.IntegerField()
-
-    class Meta:
-        verbose_name = 'Сфера работы'
-        verbose_name_plural = 'Сферы работы'
-
-    def __str__(self):
-        return self.name
     
-
-class PostLevel(models.Model):
     POST_LEVEL_NAMES = [('none', 'Не знаю'),
                         ('jun', 'Junior'),
                         ('mid', 'Middle'),
@@ -51,20 +40,17 @@ class PostLevel(models.Model):
                         ('sen', 'Senior'),
                         ('sen+', 'Senior+')]
 
-    specialization = models.ForeignKey(Specialization,
-                                       verbose_name='Сфера работы',
-                                       related_name='post_specialization',
-                                       on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, verbose_name="Название")
     code = models.IntegerField()
-    name = models.CharField(max_length=4, choices=POST_LEVEL_NAMES)
+    level_code = models.IntegerField()
+    level_name = models.CharField(max_length=4, choices=POST_LEVEL_NAMES)
 
     class Meta:
-        verbose_name = 'Уровень должности'
-        verbose_name_plural = 'Уровни должности'
+        verbose_name = 'Сфера работы'
+        verbose_name_plural = 'Сферы работы'
 
     def __str__(self):
-        return f'{self.name}, {self.specialization.name}'
-
+        return f'{self.name} {self.level_name}'
 
 class Skill(models.Model):
     SKILL_TYPE = [('soft', 'Софт скилл'), ('hard','Хард скилл')]
@@ -80,14 +66,7 @@ class Skill(models.Model):
                               related_name='skill_group',
                               on_delete=models.PROTECT)
     type = models.CharField(max_length=4, choices=SKILL_TYPE, verbose_name='Тип навыка')
-    post_level = models.ForeignKey(PostLevel,
-                                   verbose_name='Уровень должности',
-                                   related_name='post_level', 
-                                   on_delete=models.SET_NULL,
-                                   null=True, blank=True)
-                                 #  limit_choices_to={'specialization': specialization})
     code = models.IntegerField()
-
     date_from = models.DateField(verbose_name='Дата начала действия навыка')
     date_to = models.DateField(verbose_name='Дата окончания действия навыка', default=None, null=True, blank=True)
     resource_library = models.ManyToManyField(ResourceLibrary,
