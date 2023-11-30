c51ac583cefa0d6e039e816e9999493fa5a1e4ee
Ярослав Андреев <1frost34eone@gmail.com>
2023-11-30 11:12:37 +0300

    ruff fix

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,17 +1,20 @@
 from rest_framework import serializers
 
-from skills.models import (Skill )
-from users.models import ( UserSkill)
-
+from skills.models import Skill
+from users.models import UserSkill
 
 
 class SkillSerializer(serializers.ModelSerializer):
+    """Добавить описание."""
 
     class Meta:
         model = Skill
         fields = "__all__"
 
+
 class UserSkillSerializer(serializers.ModelSerializer):
+    """Добавить описание."""
+
     skill = SkillSerializer()
 
     class Meta:
