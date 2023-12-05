6f2d4e776b4da590b1262c78787566a65ef399d1
Ярослав Андреев <1frost34eone@gmail.com>
2023-12-05 19:43:14 +0300

    fixed static

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -180,6 +180,10 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
